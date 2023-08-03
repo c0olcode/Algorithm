563afb84c4340a03b6d3ee3671e9df706e79f099
Yejin Song <yjyjsssong@gmail.com>
2023-08-03 23:34:30 +0900

    Create 문자열 내 마음대로 정렬하기.java

---
+++ 프로그래머스/lv1/문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        
+        for (int i = 0 ; i < strings.length; i++){
+            strings[i]=strings[i].charAt(n)+strings[i];
+        }
+        
+        Arrays.sort(strings);
+        String[] answer = new String[strings.length];
+        
+        for (int i = 0; i < strings.length; i++){
+            answer[i] = strings[i].substring(1);
+        }
+        
+        return answer;
+    }
+}
