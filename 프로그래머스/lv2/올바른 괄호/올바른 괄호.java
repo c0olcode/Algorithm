f6dc78c8cd5d75dc78e8aa1fadf13a8e73bed724
Yejin Song <yjyjsssong@gmail.com>
2023-08-04 23:56:32 +0900

    Create 올바른 괄호.java

---
+++ 프로그래머스/lv2/올바른 괄호/올바른 괄호.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        
+        //큐 선언
+        Queue<Character> que = new LinkedList<>();
+        
+        for (int i =0; i< s.length(); i++){
+            
+            if (s.charAt(0) == ')'){
+                return false;
+            }
+            if (s.charAt(i) == '('){
+                que.add(s.charAt(i)); // (면 add
+            }
+            else{
+                if (que.size()==0) {
+                    return false;}
+                que.poll(); //)면 삭제
+            }            
+        }
+        
+        if (que.size()==0){
+            return true;
+        }
+        else return false;
+        
+    }
+}
