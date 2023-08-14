d16c1a2ddf6e7e59b1409018cd387bb7efbcd900
Yejin Song <yjyjsssong@gmail.com>
2023-08-14 20:53:42 +0900

    [level 2] Title: 더 맵게, Time: 1490.11 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,24 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-
-        for(int i = 0; i < scoville.length; i++)
-            q.add(scoville[i]);
-
-        int count = 0;
-        while(q.size() > 1 && q.peek() < K){
-            int weakHot = q.poll();
-            int secondWeakHot = q.poll();
-
-            int mixHot = weakHot + (secondWeakHot * 2);
-            q.add(mixHot);
-            count++;
+        
+        // 스코빌 지수를 우선순위 큐에 담기 (순서대로 정렬)
+        // 앞서 있는 원소가 K보다 작으면 remove해서 섞은 후 다시 큐에 담기 -> 반복
+        // 1번 시행할 때마다 return값 +1씩 해주기
+        // 다 수행했어도 k보다 작으면 -1 return
+        // count=0으로 해놓고, 애초에 비교할 필요 없으면 count return하기...
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i:scoville){
+            pq.add(i); //우선순위 큐에 스코빌 지수 담기
         }
-
-        if(q.size() <= 1 && q.peek() < K)
-            count = -1;
-
+        
+        int count = 0; //return할 값 담을 변수
+        
+        for(int i = 0; i<scoville.length; i++){ 
+            if (pq.size()>=2 && pq.peek() < K){ //pq사이즈가 2 이상이고 첫번째 원소가 k보다 작을 때
+                int first = pq.remove();
+                int second = pq.remove(); 
+                pq.add(first+second*2); //스코빌 지수 다시 계산해서 add
+                count++;
+            }else if (pq.peek()>=K){ //첫번째 원소가 K 이상일 때
+                return count;
+            }else{ //첫번째 원소가 k보다 작을 때(+pq사이즈가 0또는 1일 때)
+                return -1;
+            }
+        }
+        
         return count;
     }
 }
