c64df8342154f02d559e77c708354a7ece3b321c
Yejin Song <yjyjsssong@gmail.com>
2023-08-27 18:30:42 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -0,0 +1,30 @@
+class Solution {
+	static boolean visit[];
+
+	public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visit = new boolean[n];
+
+        for(int i=0; i<n; i++) {
+        		if(visit[i] == false) {
+                    //System.out.println(i); //0,2 /0
+        			answer++;
+        			DFS(i, computers, n);
+        		}
+        }
+
+        return answer;
+    } // End of main
+
+	static void DFS(int i, int computers[][], int n) {
+		visit[i] = true;	
+
+		for(int j=0; j<n; j++) {
+			if(visit[j] == false && computers[i][j] == 1) {
+                // System.out.println(j); // 1 / 1,2
+				DFS(j, computers, n);
+			}
+		}
+
+	} // End of DFS
+} // End of class
