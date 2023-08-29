59773db528e5030864cd780ae5eec0b1fc26161b
Yejin Song <yjyjsssong@gmail.com>
2023-08-29 16:56:05 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,45 @@
 class Solution {
-    static boolean[] visited;
-    static int answer = 0;
     
+    public static boolean visit[];
+    public int answer = 99999;
     public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length]; 
-
-        dfs(begin, target, words, 0);
-        return answer;
+        
+        // target이 words 안에 없는 경우
+        int c =0;
+        for (String word:words){
+            if (word.equals(target)) c++; // 타겟 있으면 반복문 탈출 후 계속 코드 진행
+        }
+        if (c==0) return 0; // target이 안에 없으니 0 반환
+        visit = new boolean[words.length]; //방문여부 따지기
+        dfs(begin,target,words,0); //0은 앞으로 반복되는 반복되는 단계 과정
+        
+        return answer == 999999 ? 0 : answer;
     }
     
-    public static void dfs(String begin, String target, String[] words, int cnt) {
-        //System.out.println("start dfs: cnt-> " + cnt);
-        if (begin.equals(target)) {
-            answer = cnt;
-            System.out.println("cnt-> "+ cnt);
+    public void dfs(String now, String target, String[] words, int cnt){
+        // 같아졌을 때
+        if (target.equals(now)){
+            answer = Math.min(cnt, answer);
             return;
         }
-
-        for (int i = 0; i < words.length; i++) {
-            if (visited[i]) { // 방문했을 경우
-                continue;
+        // 한글자만 다른지 확인 -> 다르다면 방문 후 false로 바꾸기.
+        for (int i =0; i < words.length; i++){
+            if (compare(now,words[i])==1 && !visit[i]){ //현재 단어와 words의 단어의 다른 부분이 1이고 방문 안했다면
+                visit[i]=true;
+                dfs(words[i],target,words,cnt+1);
+                visit[i]=false; // 다른 경로로 방문하기 위해 다시 바꿔주기. 
             }
-
-            int k = 0;    // 같은 스펠링 몇개인지 세기
-            for (int j = 0; j < begin.length(); j++) {
-                if (begin.charAt(j) == words[i].charAt(j)) {
-                    k++;
-                }
-            }
-
-            if (k == begin.length() - 1) {  // 한글자 빼고 모두 같은 경우
-                visited[i] = true; // 해당 노드 방문 가능하니까 방문표시
-                dfs(words[i], target, words, cnt + 1); // 노드 방문
-                visited[i] = false; // 방문 후에는 false
+        } 
+        return;
+    }
+    
+    public int compare(String s1, String s2){
+        int n =0;
+        for (int i = 0; i< s1.length(); i++){
+            if (s1.charAt(i) != s2.charAt(i)){
+                n++; //두 단어가 다르다면 ++
             }
         }
+        return n; //몇개 다른지 return
     }
 }
