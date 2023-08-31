9145135f05624e2c8a7e2dfc30c1e965b42991f0
Yejin Song <yjyjsssong@gmail.com>
2023-08-31 18:42:29 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,21 @@
 class Solution {
-    int answer = 0; //전역변수 선언
-    public int solution(int[] numbers, int target) { //메인 
-        // dfs 탐색 , nembers의 수=깊이까지 탐색하여서 target과 같은 수 나오면 return 값에 더해주기
+    
+    public static int sum = 0;
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
         
-        dfs(numbers,target,0,0);
-        
+        dfs(numbers, 0, 0, target);
         return answer;
     }
     
-    public void dfs(int[] numbers, int target, int depth, int sum){
+    public void dfs(int[] numbers, int depth, int sum, int target){
         if (depth == numbers.length){
-            if (sum == target) answer++;
+            if(sum == target) answer++; 
         }
         else{
-            dfs(numbers,target,depth+1,sum+numbers[depth]);
-            dfs(numbers,target,depth+1,sum-numbers[depth]);
-            
+            dfs(numbers, depth+1, sum + numbers[depth],target);
+            dfs(numbers, depth+1, sum - numbers[depth],target);
         }
+        
     }
-
-   
 }
