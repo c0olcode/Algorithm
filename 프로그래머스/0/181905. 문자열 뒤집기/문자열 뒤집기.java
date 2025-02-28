7a3c3c7e67fa65da77ff528a7b8c8bc307fa1509
Song <yjyjsssong@gmail.com>
2025-02-28 16:24:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(my_string.substring(s,e+1));
+        sb.reverse();
+        
+        sb.insert(0,my_string.substring(0,s));
+        
+        sb.append(my_string.substring(e+1));
+        
+        //String answer = "";
+        
+        return sb.toString();
+    }
+}
