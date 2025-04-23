e4bddf280142d81ed51849c81fefda7ddc91b0a6
Song <yjyjsssong@gmail.com>
2025-04-23 17:44:58 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11728 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,50 +1,57 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+    public static ArrayList<Integer> list[];
+    public static int n;
 
-  public static boolean visit[]; //방문 여부
-  public static int check[][]; //노드 여부
-  public static int count = 0; //감염된 컴퓨터 수
-  public static int node, line;
+    public static int bfs(){
+        int cnt = 0;
+        boolean visit[] = new boolean[n+1]; // 방문 여부 확인
+        Queue<Integer> q = new LinkedList<>();
 
-  public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    node = scan.nextInt();
-    line = scan.nextInt();
-    check = new int[node+1][node+1];
-    visit = new boolean[node+1];
-    
-    for (int i =0; i<line; i++){
-      int a = scan.nextInt();
-      int b = scan.nextInt();
-      check[a][b] = check[b][a] = 1;
-    }
+        // 1 넣기
+        q.add(1);
+        visit[1] = true;
 
-    int result = bfs(1);
-    System.out.println(result);
-
-    
-    scan.close();
-    
-  }
-
-  public static int bfs(int i){
-    Queue <Integer> q = new LinkedList<>();
-    q.add(i);
-    visit[i] = true;
-    int cur = i;
-    
-    while(!q.isEmpty()){
-      cur = q.poll();
-      for (int j =1 ; j<=node; j++){
-        if (check[cur][j]==1 && !visit[j]){
-          q.add(j);
-          visit[j] = true;
-          count++; 
+        while(!q.isEmpty()){
+            int v = q.poll();
+            for (int i =0; i < list[v].size(); i++){
+                int v2 = list[v].get(i);
+                if (!visit[v2]){
+                    visit[v2] = true;
+                    q.add(v2);
+                    cnt++;
+                }
+            }
         }
-      }
+        return cnt;
     }
-    return count;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken()); // 컴퓨터 갯수
+        st = new StringTokenizer(br.readLine());
+        int connect = Integer.parseInt(st.nextToken()); // 연결된 갯수
+
+        list= new ArrayList[n+1];
+
+        for (int i = 1; i <= n; i++){
+            list[i] = new ArrayList<>(); // 초기화 작업
+        }
+
+        for (int i = 0; i < connect; i++){ // 인접 리스트 생성
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+
+            list[v1].add(v2);
+            list[v2].add(v1);
+        }
+
+        System.out.println(bfs());
     }
-  
+
+
 }
