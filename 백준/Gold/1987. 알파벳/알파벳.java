6cfbfc432d6e9fa4e63b28087a437b7f692412e1
Song <yjyjsssong@gmail.com>
2025-04-24 16:50:52 +0900

    [Gold IV] Title: 알파벳, Time: 816 ms, Memory: 12080 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,63 +1,45 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static int r,c;
-    public static HashSet<Character> hs = new HashSet<>();
-    public static char map[][];
-    public static int dx[]= {0,0,-1,1};
-    public static int dy[]= {-1,1,0,0};
-    public static int max = 0;
 
-    public static boolean inRange(int x, int y){
-        return x >= 0 && y >= 0 && x < r && y <c;
-    }
+    static int r, c, max = 0;
+    static char[][] map;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
 
-    public static void dfs(int x, int y, int depth){
-//        boolean moved = false;
-
+    public static void dfs(int x, int y, int depth, int visited) {
         max = Math.max(max, depth);
-        
-        // 상하좌우 탐색 & 재귀
-        for (int i = 0; i < 4; i++){
+
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (inRange(nx,ny) && !hs.contains(map[nx][ny])){// 이동 가능하다면
-                //moved = true;
-                hs.add(map[nx][ny]);
-                dfs(nx,ny,depth+1);
-                hs.remove(map[nx][ny]);
+            if (nx >= 0 && ny >= 0 && nx < r && ny < c) {
+                int ch = map[nx][ny] - 'A';
+                if ((visited & (1 << ch)) == 0) {  // 방문하지 않은 알파벳이면
+                    dfs(nx, ny, depth + 1, visited | (1 << ch));
+                }
             }
-
-//            if(!moved){ // 더 이상 이동 불가한 경우, 해당 경로까지의 값을 기준으로 최댓값 업데이트
-//                max = Math.max(max, depth);
-//            }
         }
-
     }
 
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] rc = br.readLine().split(" ");
+        r = Integer.parseInt(rc[0]);
+        c = Integer.parseInt(rc[1]);
 
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
         map = new char[r][c];
-
-        for (int i = 0; i < r; i++){
-            String str = br.readLine();
-            for (int j = 0; j < c; j++){
-                map[i][j] = str.charAt(j);
+        for (int i = 0; i < r; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < c; j++) {
+                map[i][j] = line.charAt(j);
             }
         }
 
-        hs.add(map[0][0]);
-        dfs(0,0,1); // 상하좌우 탐색 시작.
+        int firstCharBit = 1 << (map[0][0] - 'A');
+        dfs(0, 0, 1, firstCharBit);
 
         System.out.println(max);
     }
-
-
 }
