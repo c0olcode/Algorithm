1dc888294f381833f9a47dd0973276fb55ed4d1f
Song <yjyjsssong@gmail.com>
2025-09-26 17:52:45 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,48 @@
+import java.util.*;
+
 class Solution {
-    public static int answer = 0;
-    public static int num;
-    public static boolean visit[];
+    public boolean visit[];
+    ArrayList<Integer> list[];
     
-    public static void dfs(int index, int computers[][]){
-        
-        for (int i = 0; i < num; i++){
-            // 자기 인덱스 번호면 패스
-            if(i == index) continue;
-            
-            if (computers[index][i] == 1 && !visit[i]) {
-                visit[i] = true;
-                dfs(i,computers);
+    public void dfs(int num){
+        for (int i = 0; i < list[num].size(); i++){
+            if (!visit[list[num].get(i)]) {
+                visit[list[num].get(i)] = true;
+                dfs(list[num].get(i));
             }
         }
+        
     }
-    
     public int solution(int n, int[][] computers) {
-        num = n;
+        int answer = 0;
+        
         visit = new boolean[n];
+        list = new ArrayList[n];
+        for (int i = 0; i < n; i++){
+            list[i] = new ArrayList<>();
+        }
+        
+        // 연결리스트로 만들기
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if(computers[i][j]==1){
+                    if (i==j) continue;
+                    list[i].add(j);
+                }
+            }
+        }
         
         for (int i = 0; i < n; i++){
-            if (visit[i]) continue;
-            
-            // 탐색 시작
-            visit[i] = true;
-            answer++;
-            dfs(i,computers);
+            if (!visit[i]){
+                dfs(i);
+                answer++;
+            }
         }
+        
+//         for (int i = 0; i < n; i++){
+//              System.out.println(list[i].toString());
+//         }
+        
         
         return answer;
     }
